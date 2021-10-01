8b6c4f879b943e350b9a0a3014395c2fe2d4a121
ahreum <dnflsms357@gmail.com>
2021-10-01 14:35:19 +0900

    dao 수정

--- jdbc/src/dept/DeptDAO.java
+++ jdbc/src/dept/DeptDAO.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -198,14 +197,4 @@ public class DeptDAO {
       }
       return flag;
    }
-
-	static {
-		try {
-			Class.forName("oracle.jdbc.OracleDriver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-
-	
 }
